e21113842e92aa6c4b0c142459d51c56034c1ead
Oshan Wisumperuma <warunaoshan@gmail.com>
2024-08-04 11:16:33 +1000

    add routes

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -1,9 +1,33 @@
 class ApplicationController < ActionController::Base
+  def index
+    render plain: 'Hello, world'
+  end
 
-    def index
-        # sleep 10
-        sql = "SELECT pg_sleep(3)"
-        ActiveRecord::Base.connection.execute(sql)
-        render plain: "Hello, world"
+  def cpu_intensive
+    slow_code
+    render plain: 'CPU Intensive'
+  end
+
+  def io_intensive
+    slow_query
+    render plain: 'IO Intensive'
+  end
+
+  private
+
+  def slow_query(seconds)
+    sql = "SELECT pg_sleep(#{seconds})"
+    ActiveRecord::Base.connection.execute(sql)
+  end
+
+  def slow_code
+    # 1M -> 8s in m1 mac pro
+    2_000_000.times do |i|
+      Math.sqrt(i)
     end
+  end
+
+  def block_unicorn(seconds)
+    sleep(seconds)
+  end
 end
